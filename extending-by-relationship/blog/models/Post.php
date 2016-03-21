17c580a7be6b8355e1acdee686e8ce96744671b1
arnoud <arnoud@frontendstudio.com>
2016-03-21 08:59:55 +0100

    Add extending by relationship implementation

---
+++ extending-by-relationship/blog/models/Post.php
@@ -0,0 +1,49 @@
+<?php namespace Fes\Blog\Models;
+
+use Model;
+
+/**
+ * Post Model
+ */
+class Post extends Model
+{
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'fes_blog_posts';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $belongsTo = [
+        'post' => ['RainLab\Blog\Models\Post']
+    ];
+
+    public static function getFromPost($post)
+    {
+
+
+        if ($post->post) {
+            return $post->blog;
+        }
+
+        $blog = new static;
+        $blog->post = $post;
+        $blog->save();
+        $post->post = $blog;
+        return $blog;
+
+    }
+
+}
